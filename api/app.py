e01725c25ce3053362cf63c834220f325faa482f
Bruce Gao <yexuan.gao@outlook.com>
2023-07-23 15:53:45 -0400

    cors update

--- api/app.py
+++ api/app.py
@@ -14,7 +14,7 @@ def index():
 
 
 @app.route('/bidding', methods=[ 'POST'])
-@cross_origin()
+@cross_origin(origins='*')
 def calculateScore():
     if request.method == 'POST':
         data = request.get_json()
@@ -22,7 +22,7 @@ def calculateScore():
         confidenceInterval = "(" +str(baselineInterval[0]) +"," + str(baselineInterval[1]) +")"
         bestInterval ="("+str(baselineInterval[0]) +"," + str(np.average(baselineInterval)) +"),并 靠近 "+ str(np.average(baselineInterval))
         response = jsonify({"baselineInterval": baselineInterval, "bestInterval": bestInterval, "confidenceInterval": confidenceInterval,"status": "success"})
-        response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+        #response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
         return response
     else:
         return "Please use POST"
